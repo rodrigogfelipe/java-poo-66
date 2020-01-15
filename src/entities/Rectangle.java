04f27794bb2ef40f1297c2e3994b6069a6ec9d67
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2020-01-15 15:41:27 -0300

    Informações do exercicio

--- src/entities/Rectangle.java
+++ src/entities/Rectangle.java
@@ -1,3 +1,6 @@
+/*Fazer um programa para ler os valores da largura e altura de um retângulo. Em seguida, mostrar na tela o 
+ * valor de sua área, perímetro e diagonal. Usar uma classe como mostrado no projeto ao lado.
+*/
 package entities;
 
 public class Rectangle {
